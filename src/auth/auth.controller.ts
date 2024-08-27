0791a946c93eebc177b7415313f7ab7610975a89
master-dev-khaled <khaledprogg20@gmail.com>
2024-08-27 17:52:01 +0200

    microsoft plateform backend

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,21 @@
+import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { LocalGuard } from './guards/local.guard';
+import { Request } from 'express';
+import { JwtAuthGuard } from './guards/jwt.guard';
+@Controller('auth')
+export class AuthController {
+    constructor() {}
+
+    @Post('login')
+    @UseGuards(LocalGuard)
+    login(@Req() req: Request) {
+        return req.user
+    }
+
+    @Get('status')
+    @UseGuards(JwtAuthGuard)
+    status(@Req() req: Request) {
+        return req.user
+    }
+}
---
+++ src/client/client.service.ts
@@ -0,0 +1,69 @@
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { CreateClientDto } from './dto/create-client.dto';
+import { UpdateClientDto } from './dto/update-client.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Client } from './entities/client.entity';
+import { AddressService } from 'src/address/address.service';
+import { User } from 'src/user/entities/user.entity';
+
+
+@Injectable()
+export class ClientService {
+  constructor(
+    @InjectRepository(Client) private clientRepository: Repository<Client>,
+    private addressService: AddressService
+  ) {}
+
+  async create(user, createClientDto: CreateClientDto) {
+    const {country, state, city, ...restInputs} = createClientDto
+    const newAddress = await this.addressService.create({country, state, city})
+    const newClient = await this.clientRepository.create(restInputs)
+    newClient.address = newAddress
+    newClient.user = user?.id
+    await this.clientRepository.save(newClient)
+    return {
+      success: true,
+      message: "Client created Successfully",
+      client: newClient
+    }
+  }
+
+  findAll() {
+    return this.clientRepository.find({relations: ['address', 'user']})
+  }
+
+  findOne(id: number) {
+    return this.clientRepository.findOne({where: {id}, relations: ['address', 'user']})
+  }
+  findOneByName(name: string) {
+    return this.clientRepository.findOne({where: {first_name: name}, relations: ['address', 'user']})
+  }
+
+  async update(id: number, updateClientDto: UpdateClientDto) {
+    const {country, state, city, ...restInputs} = updateClientDto
+    const client = await this.findOne(id)
+    if (!client) throw new HttpException("Client not found", HttpStatus.BAD_REQUEST)
+    await this.addressService.update(client.address?.id, {country, state, city})
+    await this.clientRepository.update({id}, {...restInputs})
+    const updatedClient = await this.findOne(id)
+    return {
+      success: true,
+      message: "Client updated Successfully",
+      client: updatedClient
+    }
+  }
+
+  async remove(id: number) {
+    const client = await this.findOne(id)
+    if (!client) throw new HttpException("client not found", HttpStatus.BAD_REQUEST)
+    await this.clientRepository.delete({id})
+    await this.addressService.remove(client.address?.id)
+    return {
+      success: true,
+      message: 'Client deleted sucessfully'
+    }
+  }
+
+  
+}
